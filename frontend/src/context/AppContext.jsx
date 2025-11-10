ce484eac0f9ea77a0f79a6e94febb4682a6b990f
HarshBharti17 <harshbharti179@gmail.com>
2025-11-10 22:26:15 +0530

    add moment.js dependency and fix import statement in AppContext

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { dummyChats, dummyUserData } from "../assets/assets"
+import { dummyChats, dummyUserData } from "../assets/assets";
 
 const AppContext = createContext();
 
