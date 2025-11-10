78d5c50c45ddd118f658c3ebea1d24496c224a8d
HarshBharti17 <harshbharti179@gmail.com>
2025-11-10 22:33:35 +0530

    create a community images components

--- frontend/src/pages/Community.jsx
+++ frontend/src/pages/Community.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { dummyPublishedImages } from "../assets/assets";
 import Loading from "./Loading";
+ 
 
 const Community = () => {
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  
   const fetchImages = async () => {
     setImages(dummyPublishedImages);
     setLoading(false);
